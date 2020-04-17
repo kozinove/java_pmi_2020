46a5da603e88a379eb6458594d70145c32df1bce
olesya zolotareva <33698001+lesya89@users.noreply.github.com>
2020-04-17 12:07:03 +0300

    add_file

---
+++ lab3/zolotareva/src/main/java/server/Server.java
@@ -0,0 +1,82 @@
+package server;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Олеся
+ */
+
+public class Server {
+    int port = 3124;
+    InetAddress ip = null;
+    ArrayList<ClientThread> allClients = new ArrayList<>();
+    int sayYes = 0;
+    int sayNo = 0;
+    
+    public void bcast(String s) {
+        for(ClientThread client : allClients) {
+            client.send(s);
+        }
+    } 
+    
+    synchronized public boolean vote(String s) {
+        if(s.equals("yes")) 
+            ++sayYes;
+        
+        if(s.equals("no"))
+            ++sayNo;
+        
+        boolean res = (sayYes + sayNo) == allClients.size();
+        
+        return  res;
+    }
+    
+    
+    synchronized public String votingRes(){ 
+        String result = "# Voting result:\nYes - " +  Integer.toString(sayYes) + '\n'  
+                         + "No - " + Integer.toString(sayNo) + '\n' ;
+        sayYes = sayNo = 0;
+        return result;
+    }
+    
+    synchronized public int clientsNum() {
+        return allClients.size();
+    }
+    
+    public Server() {
+        ServerSocket ss;
+        Socket cs;
+        try {
+            ip = InetAddress.getLocalHost();
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            ss = new ServerSocket(port, 0, ip);
+            System.out.append("Server started\n");
+            
+            while(true){
+                cs = ss.accept();
+                allClients.add(new ClientThread(cs, this));
+                System.out.append("Client connected\n");
+                System.out.append(Integer.toString(allClients.size()) + '\n');
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.append("Starting...\n");
+        new Server();
+    }
+}
